37c0e06d7bc48830855935d9b6821a484aa3cc62
Sergio Can <gersiopumas@gmail.com>
2019-06-09 23:41:30 -0500

    bug fix

--- Computadora.py
+++ Computadora.py
@@ -18,48 +18,48 @@ class Computer:
         self.cpu.getRegisters().displayRegisters()
         print("PC =", hex(self.cpu.getPC().getValue()))
         print("SP =", hex(self.cpu.getSP().getValue()))
-        instruction=input(">").upper()
+        instruction = input(">").upper()
         if instruction == "Q":
             self.cpu.setStatus(True)  # halt
         elif instruction == "A":
-            value=int(input("A="))
-            self.cpu.setRegisters("A", value)
+            value = int(input("A="))
+            self.cpu.setRegister("A", value)
         elif instruction == "B":
-            value=int(input("B="))
-            self.cpu.setRegisters("B", value)
+            value = int(input("B="))
+            self.cpu.setRegister("B", value)
         elif instruction == "C":
-            value=int(input("C="))
-            self.cpu.setRegisters("C", value)
+            value = int(input("C="))
+            self.cpu.setRegister("C", value)
         elif instruction == "D":
-            value=int(input("D="))
-            self.cpu.setRegisters("D", value)
+            value = int(input("D="))
+            self.cpu.setRegister("D", value)
         elif instruction == "E":
-            value=int(input("E="))
-            self.cpu.setRegisters("E", value)
+            value = int(input("E="))
+            self.cpu.setRegister("E", value)
         elif instruction == "H":
-            value=int(input("H="))
-            self.cpu.setRegisters("H", value)
+            value = int(input("H="))
+            self.cpu.setRegister("H", value)
         elif instruction == "L":
-            value=int(input("L="))
-            self.cpu.setRegisters("L", value)
+            value = int(input("L="))
+            self.cpu.setRegister("L", value)
         elif instruction == "P":
-            value=int(input("PC="))
+            value = int(input("PC="))
             self.cpu.setPC(value)
         elif instruction == "S":
-            value=int(input("SP="))
+            value = int(input("SP="))
             self.cpu.setSP(value)
         elif instruction == "M":
-            addr=int(input("Dir="))
+            addr = int(input("Dir="))
             self.memory.getMemory(addr)
-            value=int(input("Valor="))
+            value = int(input("Valor="))
             self.memory.setMemory(addr, value)
         elif instruction == "W":
-            filename=input("Nombre del archivo")
+            filename = input("Nombre del archivo")
             self.memory.Write(filename)
         else:
             self.cpu.execute(self.memory)
 
 
 if __name__ == "__main__":
-    c=Computer()
+    c = Computer()
     c.execute()
